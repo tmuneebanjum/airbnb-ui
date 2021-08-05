aaea2e65f365bc6e6239dfdcad45f6eb9a4e5145
Muneeb Anjum <alian@Muneebs-MacBook-Air.local>
2021-08-05 19:35:14 +0500

    Day 1 Build

---
+++ components/Footer.js
@@ -0,0 +1,40 @@
+function Footer() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100">
+      <div className="space-y-4 text-gray-800">
+        <h5 className="font-bold">About</h5>
+        <p>Airbnb Trail</p>
+        <p>Airbnb Trail</p>
+        <p>Airbnb Trail</p>
+        <p>Airbnb Trail</p>
+        <p>Airbnb Trail</p>
+      </div>
+      <div className="space-y-4 text-gray-800">
+        <h5 className="font-bold">Host</h5>
+        <p>Airbnb Trail</p>
+        <p>Airbnb Trail</p>
+        <p>Airbnb Trail</p>
+        <p>Airbnb Trail</p>
+        <p>Airbnb Trail</p>
+      </div>
+      <div className="space-y-4 text-gray-800">
+        <h5 className="font-bold">Sport</h5>
+        <p>Airbnb Trail</p>
+        <p>Airbnb Trail</p>
+        <p>Airbnb Trail</p>
+        <p>Airbnb Trail</p>
+        <p>Airbnb Trail</p>
+      </div>
+      <div className="space-y-4 text-gray-800">
+        <h5 className="font-bold">Community</h5>
+        <p>Airbnb Trail</p>
+        <p>Airbnb Trail</p>
+        <p>Airbnb Trail</p>
+        <p>Airbnb Trail</p>
+        <p>Airbnb Trail</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
